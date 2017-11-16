a584f108a656c62590db0dd9f779aeabc1c817ab
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2017-11-16 14:41:44 +0700

    change return promise to coroutines

--- libs/weather.js
+++ libs/weather.js
@@ -1,11 +1,10 @@
 const axios = require('axios');
+const Promise = require('bluebird');
 
-function check({ lat, lng }) {
-    return new Promise((resolve, reject) => {
-        axios.get(`https://api.darksky.net/forecast/ab2a079d6d2674083e4726a23e65b7fc/${lat},${lng}`)
-        .then(res => resolve(res.data.currently))
-        .catch(err => reject('an error occured while getting weather data'))
-    })
-}
+const check = Promise.coroutine(function* ({ lat, lng }) {
+    return yield axios.get(`https://api.darksky.net/forecast/ab2a079d6d2674083e4726a23e65b7fc/${lat},${lng}`)
+        .then(res => res.data.currently)
+        .catch(err => 'an error ocured')
+});
 
 module.exports = { check }
